0f69ec6a7a3632a97165033084eb8877626c9c91
Steven Zhang <YZhang@lcs.on.ca>
2021-02-20 11:01:50 -0500

    Initial Commit

---
+++ JSonData/JSonDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSonDataApp.swift
+//  JSonData
+//
+//  Created by Steven Zhang on 2021-02-20.
+//
+
+import SwiftUI
+
+@main
+struct JSonDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
