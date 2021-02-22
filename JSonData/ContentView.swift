cf0532ebf91a8fba418bf41e1b0d82d05cdb8866
Steven Zhang <YZhang@lcs.on.ca>
2021-02-22 11:12:49 -0500

    make the search view bar at the top of the app

--- JSonData/ContentView.swift
+++ JSonData/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: Stored properties
+    
+    //keep track of what what the user search for
+    @State private var searchText = ""
+    
+    //Keep the list of songs retrieve from apple music
+    @State private var songs: [Song] = []
+    
+    //MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            SearchBarView(text: $searchText)
+                .onChange(of: searchText) { _ in
+                    fetchSongResult()
+                }
+            
+            //Show a prompt when no search text given
+            if searchText.isEmpty {
+                
+                Spacer()
+                
+                Text("Please enter an artist name")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+                
+            } else {
+                
+                //Search text was given, results obtained
+                //Show the list of result
+                //Keypath of \.trackid tells us the list view what property to use
+                List(songs, id: \.trackId ) {currentSong in
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(currentSong.trackName)
+                        
+                        Text(currentSong.artistName)
+                            .font(.caption)
+                        
+                    }
+                    
+                }
+                
+            }
+        }
     }
 }
 
---
+++ JSonData/JSon.swift
@@ -0,0 +1,15 @@
+//
+//  JSon.swift
+//  JSonData
+//
+//  Created by Steven Zhang on 2021-02-21.
+//
+
+import Foundation
+
+struct JSon: Decodable {
+    
+    let message: String
+    let status: String
+    
+}
